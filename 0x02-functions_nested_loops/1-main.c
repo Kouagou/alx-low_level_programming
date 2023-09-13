22f81a509ff5cf63665b309831ece73c5b85d948
Kouagou Kenneth <ntchakenneth@gmail.com>
2023-09-12 17:12:13 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - Call print_alphabet function.
+ *
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
