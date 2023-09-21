68c0ce833647430cecf2faf13c44a0dbb3987273
Kouagou Kenneth <ntchakenneth@gmail.com>
2023-09-20 18:42:20 -0700

    Task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * cap_string - A function that capitalizes all words of a string.
+ * @str: the string to be capitalized
+ *
+ * Return: return the string after capitalized all his words.
+ */
+char *cap_string(char *str)
+{
+	int i, j, num, nLength, sLength;
+	char seps[] = {32, 9, 10, 44, 59, 46, 63, 33, 34, 40, 41, 123, 125};
+
+	nLength = strlen(str);
+	sLength = sizeof(seps) / sizeof(char);
+
+	for (j = 0; j < nLength; j++)
+	{
+		num = 0;
+		for (i = 0; i < sLength; i++)
+		{
+			if (str[j] == seps[i])
+			{
+				num = 1;
+				break;
+			}
+		}
+
+		if (num == 1 && isalpha(str[j + 1]) && islower(str[j + 1]))
+		{
+			str[j + 1] -= 32; 
+		}
+	}
+	return (str);
+}
