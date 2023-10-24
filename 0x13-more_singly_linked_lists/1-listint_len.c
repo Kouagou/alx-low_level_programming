6f18e3ad12965d0d2c88227b5a94d01690393a28
Kouagou Kenneth <ntchakenneth@gmail.com>
2023-10-24 16:29:29 -0700

    Task 1. List length

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * listint_len - A function that counts the number of nodes in a listint_t list.
+ * @h: head of the list
+ *
+ * Return: the number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	const listint_t *selector = h;
+	size_t count = 0;
+
+	while (selector != NULL)
+	{
+		count += 1;
+		selector = selector->next;
+	}
+	return (count);
+}
