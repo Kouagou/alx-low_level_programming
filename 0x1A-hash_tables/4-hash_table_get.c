057335b68c85a8e657fe900a7445a05dc51940ad
Kouagou Kenneth <ntchakenneth@gmail.com>
2023-12-22 15:25:04 -0800

    Task 4. >>> ht['betty']

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,20 +10,19 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int index, i;
+	unsigned long int index;
+	hash_node_t *found_node;
 
 	if (ht == NULL || key == NULL || key[0] == '\0')
 		return (NULL);
 
 	index = key_index((const unsigned char *)key, ht->size);
-	for (i = index; ht->array[i]; i++)
-	{
-		if (strcmp(ht->array[i]->key, key) == 0)
-		{
-			return (ht->array[i]->value);
-		}
-	}
 
-	return (NULL);
+	found_node = ht->array[index];
+	while (found_node != NULL && strcmp(found_node->key, key) != 0)
+		found_node = found_node->next;
+	if (found_node == NULL)
+		return (NULL);
 
+	return (found_node->value);
 }
